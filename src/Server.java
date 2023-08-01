c0bffa887895021bc03dad522034407559466e6b
Hdale <EkaterinaR4@yandex.ru>
2023-08-01 15:58:01 +0300

    server delete more

--- src/Server.java
+++ src/Server.java
@@ -22,37 +22,50 @@ public class Server {
     }
 
     public void startService(){
-        System.out.println("Server: startin service");
+        System.out.println("Server: starting service");
         Thread thr = new Thread(){
             public void run(){
                 System.out.println("Server: connection accept thread run");
                 while (true) {
+                    System.out.println("Server: waiting fro client connection");
                     try (Socket socket = serverSocket.accept()){
                         System.out.println("Server: accepted connection from client");
 
 
                         try {
-                            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-                            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                            while(socket != null && socket.isConnected() && !socket.isClosed()) {
+                                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+                                ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
 
-                            Request req = (Request) objectInputStream.readObject();
+                                Request req = (Request) objectInputStream.readObject();
 
-                            System.out.println("Server: received request"+req.requestName());
+                                System.out.println("Server: received request" + req.requestName());
 
-                            try {
+                                try {
+                                    System.out.println("Server: processing request");
 
-                                if (req instanceof DropRecRequest) {
-                                    DropRecRequest dropReq = (DropRecRequest) req;
-                                    mt.dropRecord(dropReq.id);
+                                    if (req instanceof DropRecRequest) {
+                                        DropRecRequest dropReq = (DropRecRequest) req;
+                                        mt.dropRecord(dropReq.id);
+                                    }
+
+                                    System.out.println("Server: request processed");
+                                } finally {
+                                    System.out.println("Server: sending response");
+                                    objectOutputStream.writeObject(new EmptyResponse());
+                                    System.out.println("Server: flushing socket output");
+                                    objectOutputStream.flush();
                                 }
-                            }finally{
-                                objectOutputStream.writeObject(new EmptyResponse());
-                                objectOutputStream.flush();
+
                             }
-
-
                         }catch(Exception e2){
                             e2.printStackTrace();
+                        } finally {
+                            System.out.println("Server: client socket communication loop is broken, finilising");
+                            if(socket != null) {
+                                System.out.println("Server: closing socket");
+                                socket.close();
+                            }
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
